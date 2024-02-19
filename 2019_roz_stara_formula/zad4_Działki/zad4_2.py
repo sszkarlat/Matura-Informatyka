05e788adf75f63b04dbd75615d315d0a25c59ca0
sszkarlat <sszkarlat@student.agh.edu.pl>
2024-02-19 20:45:55 +0100

    Completed task 4.2 & 4.3

--- 2019_roz_stara_formula/zad4_Działki/zad4_2.py
+++ 2019_roz_stara_formula/zad4_Działki/zad4_2.py
@@ -8,22 +8,19 @@ with open("przyklad.txt") as f:
             dzialki.append(dzialka)
             dzialka = []
 
-lista = []
+podobneDzialki = []
 for i in range(len(dzialki)):
     dzialka = dzialki[i]
-    print(dzialka)
     for j in range(i + 1, len(dzialki) - 1):
         k1 = 0
         k2 = len(dzialki[i]) - 1
 
         while dzialka[k1] == dzialki[j][k2][::-1]:
             k1 += 1
-            print(k1)
             k2 -= 1
-            print(k2)
 
             if (k1 == len(dzialka) and k2 == -1):
-                lista.append((i + 1, j + 1))
+                podobneDzialki.append((i + 1, j + 1))
                 break
 
-print(lista)
+print(podobneDzialki)
--- 2019_roz_stara_formula/zad4_Działki/zad4_3.py
+++ 2019_roz_stara_formula/zad4_Działki/zad4_3.py
@@ -1,6 +1,6 @@
 dzialki = []
 dzialka = []
-with open("przyklad.txt") as f:
+with open("dzialki.txt") as f:
     for i in f.read().splitlines():
         if i != "":
             dzialka.append(i)
@@ -9,27 +9,39 @@ with open("przyklad.txt") as f:
             dzialka = []
 
 
-przeszkodyWszystkie = []
-przeszkody = []
-for k in range(len(dzialki)):
-    dzialka = dzialki[k]
-    przeszkoda = (0, 0)
-    flag = "false"
-    index = 1
-    if dzialka[i][i] == "X":
-        przeszkoda = (i, j)
-        flag = "true"
-    else:
-        i += 1
-        print(i, j)
-        index += 1
-    przeszkodyWszystkie.append(przeszkody)
-print(przeszkodyWszystkie)
+def znalezieniePrzeszkod(dzialki):
+    przeszkodyWszystkie = []
+    for i in range(len(dzialki)):
+        przeszkody = []
+        dzialka = dzialki[i]
+        for j in range(len(dzialka)):
+            for k in range(len(dzialka[j])):
+                if dzialka[j][k] == "X":
+                    przeszkody.append((j, k))
+        przeszkodyWszystkie.append(przeszkody)
+    return przeszkodyWszystkie
 
-# tmp = 0
-# index = 0
-# for i in range(len(przeszkodyWszystkie)):
-#     if max(przeszkodyWszystkie[i]) > tmp:
-#         tmp = max(przeszkodyWszystkie[i])
-#         index = i + 1
-# print(tmp, index)
+
+def obliczaniePotencjalnychKwadratow(przeszkody):
+    boki = []
+    for przeszkoda in przeszkody:
+        if przeszkoda[0] > przeszkoda[1]:
+            bok = przeszkoda[0]
+        else:
+            bok = przeszkoda[1]
+        boki.append(bok)
+    return min(boki)
+
+
+przeszkodyWszystkie = znalezieniePrzeszkod(dzialki)
+bokDlaDzialki = {
+    i + 1: obliczaniePotencjalnychKwadratow(przeszkodyWszystkie[i])
+    for i in range(len(przeszkodyWszystkie))
+}
+
+maxBok = max(bokDlaDzialki.values())
+print(f"Najdłuższy bok: {maxBok}")
+
+for dzialka in bokDlaDzialki:
+    if bokDlaDzialki[dzialka] == maxBok:
+        print(f"Nr działki: {dzialka}")
